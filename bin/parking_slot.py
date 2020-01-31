c595095521df974a9d125d1c48907352534e912a
siddhant3030 <ssiddhant3030@gmail.com>
2020-01-31 22:47:30 +0530

    file input

--- bin/parking_slot.py
+++ bin/parking_slot.py
@@ -1,4 +1,24 @@
-import os, sys
+import os
+import sys
 import parking
 
+
 class ParkingCommands(object):
+    def __init__(self):
+        self.parking = parking.Parking()
+
+    def process_file(self, given_file):
+        if not os.path.exists(given_file):
+            print "Given file %s does not exist" % given_file
+
+        file_obj = open(given_file)
+        try:
+            while True:
+                line = file_obj.next()
+                if line.endswith('\n'): line = line[:-1]
+                if line == '': continue
+                self.process_command(line)
+        except StopIteration:
+            file_obj.close()
+        except Exception as ex:
+            print "Error occured while processing file %s" % ex
