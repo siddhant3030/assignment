898033cea59448e6d164e8d252913c6bf7f260b4
siddhant3030 <ssiddhant3030@gmail.com>
2020-01-31 22:56:09 +0530

    park testing

--- tests/test_parking_lot.py
+++ tests/test_parking_lot.py
@@ -12,3 +12,14 @@ def test_a_create_parking_lot(self):
         parking_slots = 6
         self.parking.create_parking_lot(parking_slots)
         self.assertEqual(len(self.parking.slots), parking_slots, msg="Wrong parking lot created")
+
+def test_b_park(self):
+        reg_no = "MH-12-FF-2017"
+        colour = "Black"
+        self.parking.park(reg_no, colour)
+        self.assertFalse(self.parking.slots[self.allocated_slot].available, "Park failed.")
+        for i in self.parking.slots.values():
+            if not i.available and i.car:
+                self.assertEqual(i.car.reg_no, reg_no, "Park failed")
+                self.assertEqual(i.car.colour, colour, "Park failed")
+
