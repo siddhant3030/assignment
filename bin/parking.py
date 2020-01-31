4715704b9e85790acbcb477dc50310764b81c622
siddhant3030 <ssiddhant3030@gmail.com>
2020-01-31 22:36:05 +0530

    this method will create parking lot

--- bin/parking.py
+++ bin/parking.py
@@ -4,3 +4,20 @@ class Parking(object):
 
     def __init__(self):
         self.slots = {}
+
+    def create_parking_lot(self, no_of_slots):
+        no_of_slots = int(no_of_slots)
+
+        if len(self.slots) > 0:
+            print "Parking Lot already created"
+            return
+
+        if no_of_slots > 0:
+            for i in range(1, no_of_slots+1):
+                temp_slot = lot.PSlot(slot_no=i,
+                                    available=True)
+                self.slots[i] = temp_slot
+            print "Created a parking lot with %s slots" % no_of_slots
+        else:
+            print "Number of slots provided is incorrect."
+        return
