ed5424a1d3e451b7439d31512b2a7897effd6ded
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-03 18:48:52 +0530

    fixes

--- bin/parking.py
+++ bin/parking.py
@@ -1,19 +1,11 @@
 import lot, car
 
 class Parking(object):
-    """
-    Parking class which has details about parking slots
-    as well as operation performed on parking are present here
-    """
-
+ 
     def __init__(self):
         self.slots = {}
 
     def create_parking_lot(self, no_of_slots):
-        """This method will create parking lot if not present already with given
-        number of slots.
-        Input: no_of_slots - Integer Type
-        """
         no_of_slots = int(no_of_slots)
 
         if len(self.slots) > 0:
@@ -31,26 +23,18 @@ class Parking(object):
         return
 
     def get_nearest_available_slot(self):
-        """Method to find nearest available slot in parking
-        """
         available_slots = filter(lambda x: x.available, self.slots.values())
         if not available_slots:
             return None
         return sorted(available_slots, key=lambda x: x.slot_no)[0]
 
     def park(self, reg_no, colour):
-        """Method to park a coming car in nearest available parking
-        slot. If not present it will throw message.
-        Input: reg_no - String Type
-               colour - String Type
-        """
 
         if not self._do_primary_checks():
             return
 
         available_slot = self.get_nearest_available_slot()
         if available_slot:
-            # create car object and save in the available slot
             available_slot.car = car.Car.create(reg_no, colour)
             available_slot.available = False
             print "Allocated slot number: %s" % available_slot.slot_no
@@ -58,9 +42,6 @@ class Parking(object):
             print "Sorry, parking lot is full."
 
     def leave(self, slot_no):
-        """Method to empty a parking slot while car is leaving.
-        Input: slot_no - Integer Type
-        """
         slot_no = int(slot_no)
         if not self._do_primary_checks():
             return
@@ -77,8 +58,6 @@ class Parking(object):
             print "Sorry, slot number does not exist in parking lot."
 
     def status(self):
-        """Method to show current status of parking
-        """
 
         if not self._do_primary_checks():
             return
@@ -95,11 +74,6 @@ class Parking(object):
         return True
 
     def registration_numbers_for_cars_with_colour(self, colour):
-        """Method to find registration numbers of car with given colour in
-        parking
-        Input: colour - String Type
-        """
-
         if not self._do_primary_checks():
             return
 
@@ -115,10 +89,6 @@ class Parking(object):
             print "Not found"
 
     def slot_numbers_for_cars_with_colour(self, colour):
-        """Method to find slot numbers for cars with given colour in
-        parking.
-        Input: colour - String Type
-        """
 
         if not self._do_primary_checks():
             return
@@ -135,11 +105,6 @@ class Parking(object):
             print "Not found"
 
     def slot_number_for_registration_number(self, reg_no):
-        """Method to find slot numbers in parking with given registration
-        number.
-        Input: reg_no - String Type
-        """
-
         if not self._do_primary_checks():
             return
 
