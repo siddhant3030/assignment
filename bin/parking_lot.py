400a8579229ae4136b3555baa271c66724fd1e41
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-04 11:11:11 +0530

    bug fixes

--- bin/parking_lot.py
+++ bin/parking_lot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import os, sys
-import parking
+import parking 
 
 class ParkingCommands(object):
 
--- tests/test_parking_lot.py
+++ tests/test_parking_lot.py
@@ -2,14 +2,17 @@ import unittest
 import env
 from bin import parking
 class TestParkingLot(unittest.TestCase):
+    
     @classmethod
     def setUpClass(cls):
         cls.parking = parking.Parking()
         cls.allocated_slot = 1
+    
     def test_a_create_parking_lot(self):
-            parking_slots = 6
+            parking_slots = 10
             self.parking.create_parking_lot(parking_slots)
             self.assertEqual(len(self.parking.slots), parking_slots, msg="Wrong parking lot created")
+    
     def test_b_park(self):
             reg_no = "MH-12-FF-2017"
             colour = "Black"
@@ -19,9 +22,11 @@ class TestParkingLot(unittest.TestCase):
                 if not i.available and i.car:
                     self.assertEqual(i.car.reg_no, reg_no, "Park failed")
                     self.assertEqual(i.car.colour, colour, "Park failed")
+    
     def test_c_leave(self):
             self.parking.leave(self.allocated_slot)
             self.assertTrue(self.parking.slots[self.allocated_slot].available, "Leave failed.")
+    
     @classmethod
     def tearDownClass(cls):
         del cls.parking
