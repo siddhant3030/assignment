550e398a67cf6f583a0a3f018451ce84cc1c3fbf
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-08 00:33:18 +0530

    test

--- docs/Tests.py
+++ docs/Tests.py
@@ -18,6 +18,12 @@ class TestParkingLotUtilities(unittest.TestCase):
         testParkingLot = create_parking_lot(str(6))
         self.assertEqual(parking_lot_is_full(testParkingLot), False)
 
+    #if car is not defined
+    def test_park_car_lot_not_defined(self):
+        testString = park_car(None, 'KA-01-AA-1111', 'White')
+        self.assertEqual('Parking lot is not defined', testString)
+
+
 
 if __name__ == '__main__':
     unittest.main()
