bbb69978d6fd9870a164faffda173985e53f3c65
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-08 00:16:45 +0530

    testing the functions

---
+++ docs/Tests.py
@@ -0,0 +1,19 @@
+import unittest
+from Utilities import create_parking_lot, park_car, parking_lot_is_full, car_departure, \
+    car_by_colour, slot_by_car_number, slot_by_colour
+
+
+class TestParkingLotUtilities(unittest.TestCase):
+    """
+    will test the endpoints
+    """
+
+    def test_create_parking_lot(self):
+        testParkingLot = create_parking_lot(str(6))
+        self.assertEqual(len(testParkingLot.get_slots()), 6)
+
+  
+
+
+if __name__ == '__main__':
+    unittest.main()
